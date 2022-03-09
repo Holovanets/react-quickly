e55917d0c8a0af6864af1c0a57eabff0488e595c
ForestChannel <vip.golovanets@gmail.com>
2022-03-09 15:35:49 +0200

    71

--- ch01/hello-world/js/script.js
+++ ch01/hello-world/js/script.js
@@ -1,2 +1,32 @@
 var h1 = React.createElement('h1', null, 'Hello world!')
-ReactDOM.render(h1, document.getElementById('content'))
+var h2 = React.createElement('h1', null, 'Hello friends!')
+
+class HelloWorld extends React.Component {
+  render() {
+    console.log(Object.isFrozen(this.props))
+    return React.createElement('div', null, h1, h2)
+  }
+}
+
+ReactDOM.render(
+  React.createElement(
+    'div',
+    null,
+    React.createElement(HelloWorld, {
+      id: 'ember',
+      frameworkName: 'Ember.js',
+      title: 'A framework for creating ambitious web applications.',
+    }),
+    React.createElement(HelloWorld, {
+      id: 'backbone',
+      frameworkName: 'Backbone.js',
+      title: 'Backbone.js gives structure to web applications...',
+    }),
+    React.createElement(HelloWorld, {
+      id: 'angular',
+      frameworkName: 'Angular.js',
+      title: 'Superheroic JavaScript MVW Framework',
+    })
+  ),
+  document.getElementById('content')
+)
